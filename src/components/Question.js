a1c98be6bc6f09cdfe4075dcc3ef5bd3ce9beceb
Tani Kaye <1tanikaye@gmail.com>
2021-11-09 11:46:10 -0500

    commit tree e88258cdefc086fa25a5f12f2fbee38923b1fb51

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  console.log(timeRemaining)
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+
+const timeoutId = setTimeout(() => {
+  setTimeRemaining(timeRemaining -1)
+}, 1000)
+
+return function() {
+  console.log("cleanup")
+  clearTimeout(timeoutId)
+}
+  }, [timeRemaining])
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
